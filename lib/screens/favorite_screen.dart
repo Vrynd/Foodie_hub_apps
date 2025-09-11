8c404e8d34049e7d5ff2dc9f3a2eb38d15ce9c40
Vrynd <vryndhika@gmail.com>
2025-09-12 04:06:53 +0700

    Memperbarui UI pada menu favorite

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_app/components/app_bar.dart';
+import 'package:restaurant_app/components/header.dart';
 import 'package:restaurant_app/components/list_items.dart';
 import 'package:restaurant_app/provider/local_database_provider.dart';
 import 'package:restaurant_app/routes/app_route.dart';
@@ -27,62 +28,122 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
       backgroundColor: Theme.of(context).colorScheme.surfaceContainerLow,
       appBar: AppBarTemplate(
         title: Text(
-          'Favorite',
-          style: Theme.of(context).textTheme.titleLarge?.copyWith(
-            color: Theme.of(context).colorScheme.onSurface,
+          'FoodieHub',
+          style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+            color: Theme.of(context).colorScheme.primary,
           ),
         ),
       ),
-      body: Consumer<LocalDatabaseProvider>(
-        builder: (context, value, child) {
-          final favoriteList = value.restaurantList ?? [];
-          return favoriteList.isNotEmpty
-              ? ListView.builder(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 14,
-                    vertical: 16,
-                  ),
-                  itemCount: favoriteList.length,
-                  itemBuilder: (context, index) {
-                    final restoFav = favoriteList[index];
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 16),
-                      child: ListItems(
-                        restaurant: restoFav,
-                        onTap: () {
-                          Navigator.pushNamed(
-                            context,
-                            NavigationRoute.detailRoute.name,
-                            arguments: restoFav.id,
-                          );
-                        },
-                      ),
+      body: SafeArea(
+        child: Consumer<LocalDatabaseProvider>(
+          builder: (context, value, child) {
+            final favoriteList = value.restaurantList ?? [];
+            if (favoriteList.isNotEmpty) {
+              return ListView.builder(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 14,
+                  vertical: 16,
+                ),
+                itemCount: favoriteList.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Header(
+                          title: 'Your Favorites',
+                          subtitle: 'All the restaurants you liked',
+                        ),
+                        const SizedBox(height: 16),
+                      ],
                     );
-                  },
-                )
-              : Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.favorite_border,
-                        size: 80,
-                        color: Theme.of(context).colorScheme.onSurfaceVariant,
-                      ),
-                      const SizedBox(height: 16),
-                      Text(
-                        "No favorite yet",
-                        style: Theme.of(context).textTheme.titleMedium
-                            ?.copyWith(
-                              color: Theme.of(
-                                context,
-                              ).colorScheme.onSurfaceVariant,
-                            ),
-                      ),
-                    ],
+                  }
+
+                  final restoFav = favoriteList[index - 1];
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 16),
+                    child: ListItems(
+                      restaurant: restoFav,
+                      onTap: () {
+                        Navigator.pushNamed(
+                          context,
+                          NavigationRoute.detailRoute.name,
+                          arguments: restoFav.id,
+                        );
+                      },
+                    ),
+                  );
+                },
+              );
+            } else {
+              return Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 14, vertical: 16),
+                    child: Header(
+                      title: "Your Favorites",
+                      subtitle: "Restaurants you love the most",
+                    ),
                   ),
-                );
-        },
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 14,
+                        right: 14,
+                        bottom: 16,
+                        // vertical: 16,
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Theme.of(
+                            context,
+                          ).colorScheme.surfaceContainerLowest,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.bookmarks_outlined,
+                                size: 84,
+                                color: Theme.of(
+                                  context,
+                                ).colorScheme.onSurfaceVariant,
+                              ),
+                              const SizedBox.square(dimension: 16),
+                              Text(
+                                "No favorites yet",
+                                style: Theme.of(context).textTheme.headlineSmall
+                                    ?.copyWith(
+                                      color: Theme.of(
+                                        context,
+                                      ).colorScheme.onSurface,
+                                    ),
+                              ),
+                              const SizedBox.square(dimension: 8),
+                              Text(
+                                "Start exploring restaurants and add them to your favorites!",
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context).textTheme.bodyLarge
+                                    ?.copyWith(
+                                      color: Theme.of(
+                                        context,
+                                      ).colorScheme.onSurfaceVariant,
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
+          },
+        ),
       ),
     );
   }
