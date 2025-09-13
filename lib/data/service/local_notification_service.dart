c97dd2adc838772e7f08a4b29442e4d455083a90
Vrynd <vryndhika@gmail.com>
2025-09-14 06:29:11 +0700

    Membuat layanan untuk notifikasi

---
+++ lib/data/service/local_notification_service.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+class LocalNotificationService {
+  Future<void> init() async {
+    const initializationSettings = InitializationSettings(
+      android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+      iOS: DarwinInitializationSettings(),
+    );
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<bool> _isAndroidPermissionGranted() async {
+    return await flutterLocalNotificationsPlugin
+            .resolvePlatformSpecificImplementation<
+              AndroidFlutterLocalNotificationsPlugin
+            >()
+            ?.areNotificationsEnabled() ??
+        false;
+  }
+
+  Future<bool> _requestAndroidNotificationsPermission() async {
+    return await flutterLocalNotificationsPlugin
+            .resolvePlatformSpecificImplementation<
+              AndroidFlutterLocalNotificationsPlugin
+            >()
+            ?.requestNotificationsPermission() ??
+        false;
+  }
+
+  Future<bool?> requestPermissions() async {
+    if (defaultTargetPlatform == TargetPlatform.iOS) {
+      final iOSImplementation = flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin
+          >();
+      return await iOSImplementation?.requestPermissions(
+        alert: true,
+        badge: true,
+        sound: true,
+      );
+    } else if (defaultTargetPlatform == TargetPlatform.android) {
+      final notificationEnabled = await _isAndroidPermissionGranted();
+      if (!notificationEnabled) {
+        final requestNotificationsPermission =
+            await _requestAndroidNotificationsPermission();
+        return requestNotificationsPermission;
+      }
+      return notificationEnabled;
+    } else {
+      return false;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_app/data/api/api_service.dart';
 import 'package:restaurant_app/data/database/local_database_service.dart';
+import 'package:restaurant_app/data/service/local_notification_service.dart';
 import 'package:restaurant_app/data/service/theme_preferences_service.dart';
 import 'package:restaurant_app/provider/detail_provider.dart';
 import 'package:restaurant_app/provider/favorite_icon_provider.dart';
@@ -23,6 +24,7 @@ void main() async {
   runApp(
     MultiProvider(
       providers: [
+        Provider(create: (context) => LocalNotificationService()..init()),
         Provider(create: (context) => ThemePreferencesService(prefs)),
         ChangeNotifierProvider(
           create: (context) =>
